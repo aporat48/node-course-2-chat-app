b5665b309ada63259c0335ef4296d1a35493cbb0
AviP <avi.unit1@gmail.com>
2016-12-11 11:58:46 +0200

    created a module

--- server/server.js
+++ server/server.js
@@ -2,39 +2,28 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
+const {generateMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
-
+// generateMessage();
 app.use(express.static(publicPath));
 
 io.on('connection',function (socket)  {
   console.log('new user connected');
-  socket.emit('newMessage',{
-    from: "Admin",
-    text: 'welcome to our room'
-  });
-  socket.broadcast.emit('newMessage',{
-    from: 'Admin',
-    text:'new user joined room'
-  });
-
+  socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
+  socket.broadcast.emit('newMessage',generateMessage('Admin', 'New user joined room '));
 
   socket.on('createMessage', (message) => {
     console.log('create message', message);
-    io.emit('newMessage',{
-      from: message.from,
-      text: message.text,
-      createdAt: new Date().getTime()
-    });
-  })
-  socket.on('disconnect', function () {
-  console.log('disconnected from server');
+    io.emit('newMessage',generateMessage(message.from, message.text));
   });
-});
-
+    socket.on('disconnect', () => {
+      console.log('User was disconnected');
+    });
+  });
 server.listen(port, () => {
   console.log(`Server is up on port ${port}`);
 });
